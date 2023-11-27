c66b12120cd07ccaa37e28fd01563ad778d8d8bd
Lovedeep <lovedeep@Apples-MacBook-Air.local>
2023-11-27 23:04:49 +0530

    created todo with next and TS

---
+++ src/components/TodoItems/TodoItems.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { TodoType } from "@/types";
+import { PiTrashSimpleThin } from "react-icons/pi";
+import styles from "./TodoItems.module.scss";
+
+const TodoItems = ({
+  todos,
+  todoDeleteHandler,
+}: {
+  todos: TodoType[];
+  todoDeleteHandler: (id: string) => void;
+}) => {
+  return (
+    <ul className={styles.todoItems}>
+      {todos.length ? (
+        todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <span>{todo.todo}</span>
+              <span>
+                <button onClick={() => todoDeleteHandler(todo.id)}>
+                  <PiTrashSimpleThin className={styles.deleteIcon} />
+                </button>
+              </span>
+            </li>
+          );
+        })
+      ) : (
+        <li>No Todos Found!!</li>
+      )}
+    </ul>
+  );
+};
+
+export default TodoItems;
